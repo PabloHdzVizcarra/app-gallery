6dfc0fb666c1735f00dcb2d47f35c55d6558ce42
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-06-02 18:00:16 -0500

    feat added Main component

--- src/components/AppGallery.tsx
+++ src/components/AppGallery.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.css";
 import Header from "./header/Header";
 import SearchBar from "./search_bar/SearchBar";
+import Main from "./main/Main";
 
 function AppGallery() {
   return (
       <div className="main-container">
         <Header title={"App Gallery"}/>
         <SearchBar/>
+        <Main/>
       </div>
   );
 }
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+      <main className="images">
+        Main Content
+      </main>
+  );
+}
+
+export default Main;
