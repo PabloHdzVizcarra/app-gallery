09de9e846ad602bde4dca4cc7bb4747a1c3f7a08
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-06-02 20:36:01 -0500

    refactor ImageComponent

--- src/components/main/ImageComponent.tsx
+++ src/components/main/ImageComponent.tsx
@@ -8,11 +8,11 @@ type ImageProps = {
 function ImageComponent({image}: ImageProps) {
   return (
       <div className="image">
-        <h3 className="image__title">{image.description ? image.description : "Default title"}</h3>
+        <h3 className="image__title">{image.user.name ? image.user.name : "Default title"}</h3>
         <img src={image.urls.small} alt={image.alt_description} className="image__picture"/>
         <div className="image__info">
           <small className="image__sub">{image.alt_description}</small>
-          <p className="image__creator">Created by <strong>{image.user.name}</strong></p>
+          <p className="image__creator">Created at <small>{image.created_at.slice(0, 10)}</small></p>
         </div>
       </div>
   );
