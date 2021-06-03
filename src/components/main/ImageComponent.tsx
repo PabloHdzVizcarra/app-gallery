78fc54cde24e48cf32da08baa7d243d2c390ce7d
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-06-02 19:28:30 -0500

    feat added basic css styles

--- src/components/main/ImageComponent.tsx
+++ src/components/main/ImageComponent.tsx
@@ -7,7 +7,14 @@ type ImageProps = {
 
 function ImageComponent({image}: ImageProps) {
   return (
-      <div>{image.description}</div>
+      <div className="image">
+        <h3 className="image__title">{image.description ? image.description : "Default title"}</h3>
+        <img src={image.urls.small} alt={image.alt_description} className="image__picture"/>
+        <div className="image__info">
+          <small className="image__sub">{image.alt_description}</small>
+          <p className="image__creator">Created by <strong>{image.user.name}</strong></p>
+        </div>
+      </div>
   );
 }
 
